accdd179f99253f93084b01b718859c8dc2db59e
dev-ahmedfares <ahmedfaresss416@gmail.com>
2025-03-12 11:30:47 +0200

    init

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import BtnPrimary from "./shared/BtnPrimary";
-import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
+import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Input } from "./ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string(),
+});
 
 function Subscribe() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
     <div>
       <div>
@@ -17,13 +34,13 @@ function Subscribe() {
         </p>
       </div>
       <div>
-      <Form {...form}>
+        <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <FormField
               control={form.control}
-              name="fullName"
+              name="email"
               render={({ field }) => (
-                <FormItem >
+                <FormItem>
                   <FormControl>
                     <Input
                       placeholder="Full Name..."
@@ -31,16 +48,13 @@ function Subscribe() {
                       {...field}
                     />
                   </FormControl>
-
-
                 </FormItem>
               )}
             />
-          
-           
-            <div className="flex justify-center ">
+
+            <div className="flex justify-center">
               <BtnPrimary customStyle="min-h-[50px] mt-8 !text-lg px-20">
-              Subscribe
+                Subscribe
               </BtnPrimary>
             </div>
           </form>
